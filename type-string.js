30e98a62bffdced26226b202676d3d21441e2ad1
Raoni Kirschner <90520708+RaoniKir@users.noreply.github.com>
2023-03-04 15:26:46 -0300

    Add files via upload
    Primeira Versão

---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao= "Meu nome é ";
+const meuNome = "Raoni"
+console.log(citacao)
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string OU template literal
+
