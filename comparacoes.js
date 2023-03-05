bfac1a61dbd476f6613b27ac657477ebf19d52a3
Raoni Kirschner <raoni.kirschner@gmail.com>
2023-03-05 00:01:11 -0300

    Operadores modulo 4

---
+++ comparacoes.js
@@ -0,0 +1,20 @@
+// == (comparação implícita)
+
+const numero = 8;
+const texto = "8";
+
+console.log(numero == texto)
+
+// == (comparação explícita)
+
+const numero2 = 8;
+const texto2 = "8";
+
+console.log(numero2 === texto2)
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == só compara ovalor
+// === compara valor e o tipo de dado
